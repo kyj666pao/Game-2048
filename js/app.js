09e557618a88a076b31c08bfd80696d563e0d4a0
Jerry Pao <kyjerry0424@gmail.com>
2023-03-31 23:42:37 -0400

    define randomGenerate to generate a numer in one of blank square. In the function, create a list for index of blank square, and randomly pick a index to generate, then update the board array and the Html element of square.

--- js/app.js
+++ js/app.js
@@ -16,19 +16,47 @@ init();
 function init() {
   let allSqr = document.querySelectorAll(".sqr p");
   allSqr.forEach((e) => e.remove());
-  board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  board = [0, 0, 0, 2, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0];
   win = false;
   failed = false;
+
   updateBoard();
 }
 
 function updateBoard() {
   sqrEls.forEach((e, idx) => {
-    let sqr = document.createElement("p");
+    let sqrContent = document.createElement("p");
     board[idx] == 0
-      ? ((sqr.innerHTML = " "), e.append(sqr))
-      : ((sqr.innerHTML = board[idx]), e.append(sqr));
+      ? ((sqrContent.innerHTML = " "), e.append(sqrContent))
+      : ((sqrContent.innerHTML = board[idx]), e.append(sqrContent));
   });
+}
+
+randomGenerate()
+function randomGenerate() {
+  //   console.log(board);
+  //  create a list for index of square of blank
+  let blankList = [];
+  board.forEach((e, idx) => {
+    e == 0 ? blankList.push(idx) : "";
+  });
+  //   console.log(blankList);
+  // pick a square randomly to generate the number
+  let whereGenerate = blankList[Math.floor(Math.random() * blankList.length)];
+  //   console.log(`whereGenerate: ${whereGenerate}`);
+
+  let probability = Math.floor(Math.random() * 10);
+  //   console.log(`probability: ${probability}`);
+
+  board[whereGenerate] = probability < 9 ? 2 : 4;
+  
+  let sqrContent = document.createElement("p");
+  Num = probability < 9 ? 2 : 4;
+  console.log(Num);
+  sqrContent.innerHTML = Num;
+  let sqr = document.querySelector(`#sq${whereGenerate}`);
+  sqr.append(sqrContent);
+  console.log(board);
 }
 
 function handleKeyDown(e) {
